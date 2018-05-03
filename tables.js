b885f6fa9f68336c1f8c00fa5b8d9a7828fe5583
Basavaraj Sonnad <basavaraj.sonnad@agrostar.in>
2018-05-03 17:12:47 +0530

    support of sticky header for tables added

--- tables.js
+++ tables.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { AgroList } from "./lists";
 
 export const AgroTable = AgroList.extend`margin: 0;`;
@@ -10,12 +10,29 @@ export const AgroTableRow = styled.li`
   align-items: center;
   font-weight: ${props => (props.bold ? "bold" : "inherit")};
   padding-bottom: 1em;
+  padding-top: 1em;
   margin-top: 0.5em;
   border-bottom: 1px solid ${props => props.theme.label.fainted.color};
+
+  ${props => addStickyStyle(props.sticking)}
+`;
+
+function addStickyStyle(sticking) {
+  if (!sticking) {
+    return css``;
+  }
+  return css`
+      position: fixed;
+      top: 120px;
+      width: 93%;
+  `;
+}
+
+
+export const TableContent = styled.div`
+  padding-top: 0;
 `;
 
 export const TableItem = styled.div`
   flex: ${props => (props.agSize ? props.agSize : 1)};
 `;
-
-export const AgroTableItem = TableItem;
